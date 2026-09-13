7be5ce7525bfcb32efe237a2fe0e9a5dbaa93d47
Stella Hack <foxoftheasterisk@gmail.com>
2026-09-13 14:49:06 -0700

    create animations for worm attack

---
+++ Assets/Scripts/Enemies/ProjectileAttackAI.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Weltraumsknecht.Enemies
+{
+    public class ProjectileAttackAI : AttackAI
+    {
+
+
+        public override bool IsInRange(Vector2 playerPosition)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void StartAttack()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void ContinueAttack()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
