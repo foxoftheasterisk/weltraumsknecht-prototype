894505ad83f1067cc62b2f447089497c0d1ef902
Stella Hack <foxoftheasterisk@gmail.com>
2026-09-13 14:50:57 -0700

    change AttackAI to support animations

--- Assets/Scripts/Enemies/AttackAI.cs
+++ Assets/Scripts/Enemies/AttackAI.cs
@@ -4,25 +4,70 @@ using UnityEngine;
 namespace Weltraumsknecht.Enemies
 {
 
+    [RequireComponent(typeof(Animator))]
     public abstract class AttackAI : MonoBehaviour
     {
-        public bool IsAttacking
+        public bool IsActive
         { get; protected set; } = false;
 
         public int priority = 0;
+        public string animationTriggerName;
+
+        private const string animationWarmupName = "warmupCompletion";
+
+        public float warmupTime = .3f;
+        private float elapsedTime;
+
+        private bool inAttack;
+
+        protected Animator animator;
+
+        void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
 
         public abstract bool IsInRange(Vector2 playerPosition);
 
-        public virtual void StartAttack()
+        public virtual void StartWarmup()
         {
-            IsAttacking = true;
+            IsActive = true;
+            inAttack = false;
+            elapsedTime = 0;
+            animator.SetTrigger(animationTriggerName);
+            animator.SetFloat(animationWarmupName, 0);
+        }
+
+        public void Continue()
+        {
+            if (inAttack)
+            {
+                ContinueAttack();
+            }
+            else
+            {
+                elapsedTime += Time.deltaTime;
+                animator.SetFloat(animationWarmupName, elapsedTime / warmupTime);
+                if(elapsedTime >= warmupTime)
+                {
+                    inAttack = true;
+                    StartAttack();
+                }
+            }
         }
 
         public virtual void CancelAttack()
         {
-            IsAttacking = false;
+            IsActive = false;
         }
 
-        public abstract void ContinueAttack(); //This one might not be necessary? Could use update instead.
+        protected abstract void StartAttack();
+        protected abstract void ContinueAttack();
+
+        protected void EndAttack()
+        {
+            IsActive = false;
+            inAttack = false;
+        }
     }
 }
